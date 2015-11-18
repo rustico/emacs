75d401631421daef24020dd66a72597e5a2545b1
rustico <nmbases@gmail.com>
2015-11-18 10:05:50 -0300

    Some changes with the font size, helm and magit

--- init.el
+++ init.el
@@ -27,4 +27,15 @@
 ;;; Load the config
 (global-set-key (kbd "<f2>") (lambda () (interactive) (find-file-other-window user-init-file)))
 (org-babel-load-file (concat user-emacs-directory "config.org"))
-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files (quote ("~/Dropbox/org/xively.org"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
